0d56752f33d1377b524f0fa843a612b7fd0a64fc
root <root@6744e852c185>
2020-11-24 22:51:59 +0000

    YAY

---
+++ src/_05_unit_tests/JUnitests.java
@@ -0,0 +1,20 @@
+package _05_unit_tests;
+
+import static org.junit.Assert.assertEquals;
+
+public class JUnitests {
+
+	public static void main(String[] args) {
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
---
+++ src/_06_minion/Minion.java
@@ -0,0 +1,44 @@
+package _06_minion;
+
+public class Minion {
+	/*2.
+	 So to make these tests pass, you will first need to create a Minion class with the member variables below:
+
+	  			private String name; 
+	  			private int eyes; 
+	  			private String color; 
+	  			private String master;
+
+	 3.
+	  Next, create a constructor, and getters and setters for all the member variables of the Minion class. 
+	  If they’re done right, all these tests will pass.
+	  
+	  To check, run this MinionTest class and the JUnit tab should show a GREEN bar.
+	    
+	 */
+
+	private String name; 
+		private int eyes; 
+		private String color; 
+		private String master;
+
+public Minion(String name, int eyes, String color, String master) 
+{ 
+	this.name = name;
+	this.eyes = eyes;
+	this.color = color;
+	this.master = master;
+} 
+String getName() {
+	return name;
+}
+int getEyes() {
+	return eyes;
+}
+String getColor() {
+	return color;
+}
+
+
+
+}
